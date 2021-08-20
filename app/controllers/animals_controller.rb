22a80e005bab3f23c2a379967284a6816965c7e0
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-20 09:18:18 -0700

    add animals controller to page

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,40 @@
+class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.all
+    json_response(@animals)
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+    json_response(@animal)
+  end
+
+  def create
+    @animal = Animal.create!(animal_params)
+    json_response(@animal, :created)
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update!(animal_params)
+      render status: 200, json: {
+        message: "This animal has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    if @animal.destroy!
+      render status: 200, json: {
+        message: "This animal has been successfully destroyed."
+      }
+    end
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+end
