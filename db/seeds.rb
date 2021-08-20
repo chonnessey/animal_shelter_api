0dca8179937ac85115aed81454c56c9704de847c
Adrian Camacho <adriancamacho18@gmail.com>
2021-08-20 10:07:36 -0700

    fix seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,19 +14,34 @@ class Seed
   end
 
   def generate_animals
-    20.times do |i|
-      animal = Animal.create!(
-        animal_type: "Dog" || "Cat",
-        breed: Faker::Creature::Dog.breed || Faker::Creature::Cat.breed,
+    10.times do |i|
+      dog = Animal.create!(
+        animal_type: "Dog",
+        breed: Faker::Creature::Dog.breed,
         name: Faker::JapaneseMedia::Naruto.character,
         age: Faker::Number.between(from: 1, to: 20),
-        sex: "Male" || "Female",
+        sex: "Male",
         color: Faker::Color.color_name,
         weight: Faker::Number.between(from: 5, to: 100),
         date: Faker::Date.backward(days: 365),
         adoption_fee: Faker::Number.decimal(l_digits: 2)
       )
-      puts "#{animal.animal_type} #{i}: Name is #{animal.name} and age is #{animal.age}."
+      puts "#{dog.animal_type} #{i}: Name is #{dog.name}, Breed is #{dog.breed} and age is #{dog.age}."
+    end
+
+    10.times do |d|
+      cat = Animal.create!(
+        animal_type: "Cat",
+        breed: Faker::Creature::Cat.breed,
+        name: Faker::JapaneseMedia::Naruto.character,
+        age: Faker::Number.between(from: 1, to: 20),
+        sex: "Female",
+        color: Faker::Color.color_name,
+        weight: Faker::Number.between(from: 5, to: 100),
+        date: Faker::Date.backward(days: 365),
+        adoption_fee: Faker::Number.decimal(l_digits: 2)
+      )
+      puts "#{cat.animal_type} #{d}: Name is #{cat.name}, Breed is #{cat.breed} and age is #{cat.age}."
     end
   end
 end
